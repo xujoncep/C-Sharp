36faf7f00f4e5e31a0f2b9f473a8b597c3f47e90
sujoncep <sujoncep@gmail.com>
2024-12-12 09:15:02 +0600

    master class added

---
+++ C#-MasterClass/Csharp+2024+-+01-+20+-+Datatypes/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Referency Type
+string myName = "Denis";
+
+// defining or setting up a variable
+string petsName;
+
+// initialize variable
+petsName = "Daisy";
+Console.WriteLine($"my pet is {petsName}");
+
+// assign a new value
+petsName = "Barky";
+Console.WriteLine($"my pet is {petsName}");
+
+Console.ReadKey();
+
+// Value Types
+int myAge = 35;
+//double pi = -3.14;
+
+byte age = 255;
+
+short linkdinConnections = 32550;
+
+long phoneNumber = 017657723991;
+
+float pi = 3.14f;
---
+++ Problem/SpotTest-01/SpotTest-01/Program.cs
@@ -0,0 +1,62 @@
+﻿
+//Merged two arry and then sort it and print
+
+//int[] arr1 = { 1, 2, 3 };
+//int[] arr2 = { 4, 2, 7 };
+
+//int[] arr3 = arr1.Concat(arr2).ToArray();
+//int[] oldarr = new int[6] ;
+
+//Array.Copy(arr3,oldarr,6);
+
+//Array.Sort(arr3);
+
+//int [] newarr = arr3.OrderBy(x => x)
+//                    .ToArray();
+
+//foreach (int i in arr3)
+//{
+
+//    Console.WriteLine(i);
+//}
+
+// Make every string first latter uppercase.
+
+//string[] words = { "hello", "hi" };
+
+//string[] UpperWords = words.Select( words => words[0].ToString().ToUpper() + words.Substring(1)).ToArray();
+
+//foreach (string word in UpperWords)
+//{
+//    Console.WriteLine(word);
+//}
+
+List<int> evenList = new List<int>();
+List<int> oddList = new List<int>();
+
+for(int i=1; i<10; i++)
+{
+    if (i % 2 == 0)
+    {
+        evenList.Add(i);
+    }
+
+    else
+    {
+        oddList.Add(i);
+    }
+
+}
+
+
+Console.WriteLine("Even arry:");
+foreach(int i in evenList)
+{
+    Console.Write(i +" ");
+}
+
+Console.WriteLine("\nOdd arry:");
+foreach (int i in oddList)
+{
+    Console.Write(i +" ");
+}
